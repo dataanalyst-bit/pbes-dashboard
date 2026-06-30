9dfb29aed5c4449db758eb56124b9945af0a0d35
dataanalyst-bit <dataanalyst@pallavimodelschools.org>
2026-06-30 16:05:44 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,65 +1,31 @@
-// ===============================
-// PBES Analytics Authentication
-// ===============================
-
-// Login Function
 async function login() {
-
     const email = document.getElementById("email").value.trim().toLowerCase();
     const password = document.getElementById("password").value;
-
     const error = document.getElementById("error");
     error.innerHTML = "";
-
     if(email==="" || password===""){
         error.innerHTML="Please enter Email and Password";
         return;
     }
-
-    const { data, error:loginError } = await supabase.auth.signInWithPassword({
+    const { data, error:loginError } = await sb.auth.signInWithPassword({
         email,
         password
     });
-
     if(loginError){
-
         error.innerHTML=loginError.message;
         return;
-
     }
-
     window.location.href="index.html";
-
 }
-
-
-
-// Check Login
-
 async function checkLogin(){
-
     const {
-
         data:{session}
-
-    }=await supabase.auth.getSession();
-
+    }=await sb.auth.getSession();
     if(!session){
-
         window.location.href="login.html";
-
     }
-
 }
-
-
-
-// Logout
-
 async function logout(){
-
-    await supabase.auth.signOut();
-
+    await sb.auth.signOut();
     window.location.href="login.html";
-
 }
